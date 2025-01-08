c2d42136007d748aa852a1025f3288c5491468bf
khangai-kh <khangai.khurelbaatar@dubertas.com>
2025-01-08 23:06:19 +0900

    config

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
  */
 
 import { AppRegistry } from 'react-native';
-import 'react-native-gesture-handler';
-import 'react-native-get-random-values';
 import { name as appName } from './app.json';
 import { default as App } from './src/App';
 
