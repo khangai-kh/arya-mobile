18f1700c5a4068da079def1379a50e03b1239c30
Khangai Kh <khangai.khurelbaatar@dubertas.com>
2025-04-21 19:41:48 +0800

    [Profile] Icon Spalsh screen added

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { LightBoxProvider } from '@alantoa/lightbox';
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useEffect } from 'react';
 import { LogBox } from 'react-native';
+import { LightBoxProvider } from '@alantoa/lightbox';
+import { NavigationContainer, DefaultTheme as NavigationDefaultTheme } from '@react-navigation/native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Navigation } from './navigation';
 import { theme as paperTheme } from './theme';
-import { DefaultTheme as NavigationDefaultTheme } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { persistor, store } from './redux/configureStore';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { PersistGate } from 'redux-persist/integration/react';
 import { NoInternet } from './components/NoInternet';
 import { NavigationProvider } from './contexts/NavigationContext';
+import RNBootSplash from 'react-native-bootsplash';
 
 LogBox.ignoreLogs([
   'Non-serializable values were found in the navigation state',
@@ -21,29 +21,38 @@ LogBox.ignoreLogs([
 const queryClient = new QueryClient();
 
 const App = () => {
+  useEffect(() => {
+    const init = async () => {
+      // Simulate loading time (optional)
+      await new Promise(resolve => setTimeout(resolve, 1000));
+
+      // Hide splash screen
+      await RNBootSplash.hide({ fade: true });
+    };
+
+    init();
+  }, []);
+
   return (
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <QueryClientProvider client={queryClient}>
           <GestureHandlerRootView style={{ flex: 1 }}>
-              <PaperProvider theme={paperTheme}>
-                <LightBoxProvider>
-                  <NavigationProvider> {/* Wrap with NavigationProvider */}
-                    <NavigationContainer theme={NavigationDefaultTheme}>
-                        <Navigation />
-                        <NoInternet />
-                    </NavigationContainer>
-                  </NavigationProvider>
-                </LightBoxProvider>
-              </PaperProvider>
+            <PaperProvider theme={paperTheme}>
+              <LightBoxProvider>
+                <NavigationProvider>
+                  <NavigationContainer theme={NavigationDefaultTheme}>
+                    <Navigation />
+                    <NoInternet />
+                  </NavigationContainer>
+                </NavigationProvider>
+              </LightBoxProvider>
+            </PaperProvider>
           </GestureHandlerRootView>
         </QueryClientProvider>
       </PersistGate>
     </Provider>
   );
 };
-
-
-
 
 export default App;
